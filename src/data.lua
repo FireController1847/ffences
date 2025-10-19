465d6a727ceb37cf16ac3fa594bbc24b542a101a
FireController#1847 <usfirepilot123@gmail.com>
2025-10-18 20:14:05 -0600

    Rewrite the plugin, finish the wooden fence graphics

--- src/data.lua
+++ src/data.lua
@@ -1,4 +1,3 @@
 require("prototypes.item-groups")
 require("prototypes.item.fences")
 require("prototypes.entity.fences")
-require("prototypes.recipe.fences")
--- src/prototypes/item/fences.lua
+++ src/prototypes/item/fences.lua
@@ -1,27 +1,20 @@
 local item_sounds = require("__base__.prototypes.item_sounds")
+
 data:extend({
     {
+        -- Prototype
         type = "item",
         name = "wooden-fence",
-        icon = "__ffences__/graphics/icons/wooden-fence.png",
+        localised_description = {"item-description.wooden-fence"},
+        order = "a[stone-wall]-a[wooden-fence]",
         subgroup = "ffences",
-        order = "a[wooden-fence]-a[wooden-fence]",
+
+        -- ItemPrototype
+        stack_size = 100,
+        drop_sound = item_sounds.wood_inventory_move,
+        icon = "__ffences__/graphics/icons/wooden-fence.png",
         inventory_move_sound = item_sounds.wood_inventory_move,
         pick_sound = item_sounds.wood_inventory_pickup,
-        drop_sound = item_sounds.wood_inventory_move,
-        place_result = "wooden-fence",
-        stack_size = 50
-    },
-    {
-        type = "item",
-        name = "iron-fence",
-        icon = "__base__/graphics/icons/wall.png",
-        subgroup = "ffences",
-        order = "a[stone-wall]-b[iron-fence]",
-        inventory_move_sound = item_sounds.concrete_inventory_move,
-        pick_sound = item_sounds.concrete_inventory_pickup,
-        drop_sound = item_sounds.concrete_inventory_move,
-        place_result = "iron-fence",
-        stack_size = 100
+        place_result = "wooden-fence"
     }
 })
